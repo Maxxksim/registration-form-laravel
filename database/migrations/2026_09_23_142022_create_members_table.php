9844e1e5552fa1d3b62298298149f0c6804e5725
Maksym Chahin <makximchagin@gmail.com>
2026-09-23 15:43:01 +0000

    feat: add models and their migrations

---
+++ database/migrations/2026_09_23_142022_create_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->date('birthdate');
+            $table->string('report_subject', 500);
+            $table->string('country', 100);
+            $table->string('phone', 20);
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
